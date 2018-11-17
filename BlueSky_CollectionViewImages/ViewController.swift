2df0c64c36ef33ecfe3364d3c6fc029e1838c575
claudiasatterfield <claudiasatterfield@claudias-mac-mini.local>
2018-11-16 21:48:32 -0600

    Initial Commit

---
+++ BlueSky_CollectionViewImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueSky_CollectionViewImages
+//
+//  Created by Steve Satterfield on 11/16/18.
+//  Copyright © 2018 uwf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
